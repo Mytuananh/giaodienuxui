7f66006116f44d69981821863a4115f933dccd53
Mytuananh <tuanbenguyencodon11071973@gmail.com>
2023-04-05 01:25:10 +0700

    donetrang3anđdoingtrang4

--- test.js
+++
@@ -1,9 +0,0 @@
-var dropdownToggles = document.querySelectorAll('.dropdown-toggle');
-
-dropdownToggles.forEach(function(toggle) {
-    toggle.addEventListener('click', function() {
-        this.parentNode.classList.toggle('open');
-        var dropdownMenu = this.nextElementSibling;
-        dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-    });
-});
