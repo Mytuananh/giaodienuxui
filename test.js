fa9c3f8feea28523e4426d9e5cf19bcf9b63038f
Your Name <you@example.com>
2023-04-14 18:15:10 +0700

    doing-delete

--- test.js
+++ test.js
@@ -1,124 +1,19 @@
-// Get the modal element
-const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-const btn = document.getElementById("openModalBtn");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// Get the buttons to save and cancel changes
-const saveBtn = document.getElementById("saveBtn");
-const cancelBtn = document.getElementById("cancelBtn");
-
-// Get the form and all input fields
-const form = document.querySelector("form");
-const inputFields = form.querySelectorAll("input, select");
-
-// Get the error message elements
-const projectHistoryError = document.getElementById("project-history-error");
-
-
-// Function to clear all input fields in the form
-function clearFormInputs() {
-    inputFields.forEach((field) => {
-        field.value = "";
-    });
-}
-
-// Function to validate the input fields
-function validateFormInputs() {
-    let isValid = true;
-    const projectCodeValue = document.getElementById("project-code-num").value;
-
-    if (projectCodeValue === "") {
-        projectCodeError.textContent = "Chưa nhập mã công trình!";
-        isValid = false;
-    } else {
-        projectCodeError.textContent = "";
+document.getElementById("toggleBtn").addEventListener("click", function() {
+    let deleteColumns = document.getElementsByClassName("deleteColumn");
+    for (let i = 0; i < deleteColumns.length; i++) {
+        if (deleteColumns[i].style.display === "none") {
+            deleteColumns[i].style.display = "table-cell";
+        } else {
+            deleteColumns[i].style.display = "none";
+        }
     }
+});
 
-    const projectNameValue = document.getElementById("project-name").value;
-    if (projectNameValue === "") {
-        projectNameError.textContent = "Chưa điền tên công trình";
-        isValid = false;
-    } else {
-        projectNameError.textContent = "";
-    }
-
-    const projectLocationNameValue = document.getElementById(
-        "project-location-name"
-    ).value;
-    const projectLocationXValue = document.getElementById(
-        "project-location-x"
-    ).value;
-    const projectLocationYValue = document.getElementById(
-        "project-location-y"
-    ).value;
-    if (
-        projectLocationNameValue === "" ||
-        projectLocationXValue === "" ||
-        projectLocationYValue === ""
-    ) {
-        projectLocationError.textContent = "Chưa nhập vị trí hoặc địa điểm hoặc tọa độ";
-        isValid = false;
-    } else {
-        projectLocationError.textContent = "";
-    }
-
-    return isValid;
-}
-
-// Function to save the form data
-function saveFormData() {
-    if (validateFormInputs()) {
-        const formData = new FormData(form);
-        const data = Object.fromEntries(formData.entries());
-        // Do something with the form data, e.g. send it to the server
-        console.log(data);
-        closeModal();
-        clearFormInputs();
-    }
-}
-// Function to close the modal
-function closeModal() {
-    modal.style.display = "none";
-}
-
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    closeModal();
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target === modal) {
-        closeModal();
-    }
-};
-
-// When the user clicks the save button, save the form data
-saveBtn.onclick = function () {
-    saveFormData();
-};
-
-// When the user clicks the cancel button, close the modal and clear input fields
-cancelBtn.onclick = function () {
-    closeModal();
-    clearFormInputs();
-};
-
-// When the user types into any input field, clear its error message
-inputFields.forEach((field) => {
-    field.addEventListener("input", () => {
-        const errorElement = document.getElementById(field.name + "-error");
-        if (errorElement) {
-            errorElement.textContent = "";
+let deleteBtns = document.getElementsByClassName("deleteBtn");
+for (let i = 0; i < deleteBtns.length; i++) {
+    deleteBtns[i].addEventListener("click", function() {
+        if (confirm("Bạn chắc chắn muốn xóa chứ?")) {
+            this.closest("tr").remove();
         }
     });
-});
+}
---
+++ trang3/trang3Delete.js
@@ -0,0 +1,54 @@
+document.getElementById("toggleBtn").addEventListener("click", function() {
+    let deleteColumns = document.getElementsByClassName("deleteColumn");
+    for (let i = 0; i < deleteColumns.length; i++) {
+        if (deleteColumns[i].style.display === "none") {
+            deleteColumns[i].style.display = "table-cell";
+        } else {
+            deleteColumns[i].style.display = "none";
+        }
+    }
+});
+
+document.getElementById("toggleBtn1").addEventListener("click", function() {
+    let deleteColumns = document.getElementsByClassName("deleteColumn1");
+    for (let i = 0; i < deleteColumns.length; i++) {
+        if (deleteColumns[i].style.display === "none") {
+            deleteColumns[i].style.display = "table-cell";
+        } else {
+            deleteColumns[i].style.display = "none";
+        }
+    }
+});
+
+document.getElementById("toggleBtn2").addEventListener("click", function() {
+    let deleteColumns = document.getElementsByClassName("deleteColumn2");
+    for (let i = 0; i < deleteColumns.length; i++) {
+        if (deleteColumns[i].style.display === "none") {
+            deleteColumns[i].style.display = "table-cell";
+        } else {
+            deleteColumns[i].style.display = "none";
+        }
+    }
+});
+
+document.getElementById("toggleBtn3").addEventListener("click", function() {
+    let deleteColumns = document.getElementsByClassName("deleteColumn3");
+    for (let i = 0; i < deleteColumns.length; i++) {
+        if (deleteColumns[i].style.display === "none") {
+            deleteColumns[i].style.display = "table-cell";
+        } else {
+            deleteColumns[i].style.display = "none";
+        }
+    }
+});
+
+document.getElementById("toggleBtn4").addEventListener("click", function() {
+    let deleteColumns = document.getElementsByClassName("deleteColumn4");
+    for (let i = 0; i < deleteColumns.length; i++) {
+        if (deleteColumns[i].style.display === "none") {
+            deleteColumns[i].style.display = "table-cell";
+        } else {
+            deleteColumns[i].style.display = "none";
+        }
+    }
+});
