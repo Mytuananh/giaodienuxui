2c78a5a01a44163174266aa9081a91668a55defc
Your Name <you@example.com>
2023-04-20 04:21:20 +0700

    trang 3 thieu tao moi ho so cong trinh

--- trang3/trang3-menu-ellipsis.js
+++ trang3/trang3-menu-ellipsis.js
@@ -25,3 +25,32 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+/*---------------------------------------------Ellipsis-nav----------------------------------------------------------*/
+document.addEventListener('DOMContentLoaded', function () {
+    var ellipsisMenusNav = document.querySelectorAll('.side-menu');
+
+    ellipsisMenusNav.forEach(function (ellipsisMenu) {
+        var ellipsisBtnNav = ellipsisMenu.querySelector('.ellipsis-btn-nav');
+        var menu = ellipsisMenu.querySelector('.menu-ellipsis1-nav');
+        var menuItems = ellipsisMenu.querySelectorAll('.menu-item-ell-nav');
+
+        ellipsisBtnNav.addEventListener('click', function () {
+            menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+        });
+
+        menuItems.forEach(function (menuItem) {
+            menuItem.addEventListener('click', function () {
+                menu.style.display = 'none';
+                // Thực hiện chức năng tương ứng với từng lựa chọn ở đây
+            });
+        });
+    });
+
+    document.addEventListener('click', function (event) {
+        var openMenu = document.querySelector('.menu[style="display: block;"]');
+        if (openMenu && !event.target.closest('.ellipsis-menu')) {
+            openMenu.style.display = 'none';
+        }
+    });
+});
