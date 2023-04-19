97b47380804818fa10bc51547fe758764545844b
Mytuananh <tuanbenguyencodon11071973@gmail.com>
2023-04-19 17:01:17 +0700

    done 99% trang 3

--- trang3/trang3-menu-ellipsis.js
+++ trang3/trang3-menu-ellipsis.js
@@ -1,20 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const ellipsisBtn = document.querySelector('.ellipsis-btn');
-    const menu = document.querySelector('.menu-ellipsis1');
-    const menuItems = document.querySelectorAll('.menu-item-ell');
-    ellipsisBtn.addEventListener('click', function () {
-        menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-    });
+    var ellipsisMenus = document.querySelectorAll('.m-t-3');
 
-    menuItems.forEach(function(menuItem) {
-        menuItem.addEventListener('click', function () {
-            menu.style.display = 'none';
-            // Thực hiện chức năng tương ứng với từng lựa chọn ở đây
+    ellipsisMenus.forEach(function (ellipsisMenu) {
+        var ellipsisBtn = ellipsisMenu.querySelector('.ellipsis-btn');
+        var menu = ellipsisMenu.querySelector('.menu-ellipsis1');
+        var menuItems = ellipsisMenu.querySelectorAll('.menu-item-ell');
+
+        ellipsisBtn.addEventListener('click', function () {
+            menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+        });
+
+        menuItems.forEach(function (menuItem) {
+            menuItem.addEventListener('click', function () {
+                menu.style.display = 'none';
+                // Thực hiện chức năng tương ứng với từng lựa chọn ở đây
+            });
         });
     });
+
     document.addEventListener('click', function (event) {
-        if (!ellipsisBtn.contains(event.target) && !menu.contains(event.target)) {
-            menu.style.display = 'none';
+        var openMenu = document.querySelector('.menu[style="display: block;"]');
+        if (openMenu && !event.target.closest('.ellipsis-menu')) {
+            openMenu.style.display = 'none';
         }
     });
 });
