7bd9f5dc0f1d4abf201b7997e0a0108bce5b2297
Mytuananh <tuanbenguyencodon11071973@gmail.com>
2023-04-21 01:06:07 +0700

    doing trang 4

--- trang4/trang4-sidebar-right.js
+++
@@ -1,9 +0,0 @@
-function openSide1() {
-    document.getElementById("content").style.marginRight = "30vh";
-    document.getElementById("my-side-qlct-1").style.width = "30vh";
-}
-
-function closeSide1() {
-    document.getElementById("content").style.marginRight= "0";
-    document.getElementById("my-side-qlct-1").style.width = "0";
-}
