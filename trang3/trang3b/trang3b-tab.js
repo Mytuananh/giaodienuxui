6ee92deffde377c8cd23271b7873c88c13026c7f
Mytuananh <tuanbenguyencodon11071973@gmail.com>
2023-04-24 06:20:26 +0700

    doing 3b 30%

--- trang3/trang3b/trang3b-tab.js
+++ trang3/trang3b/trang3b-tab.js
@@ -25,10 +25,48 @@ tabs.forEach((tab, index) => {
 const songList = document.querySelector('.tab-list');
 const scrollLeftButton = document.querySelector('.scroll-left');
 const scrollRightButton = document.querySelector('.scroll-right');
+const songList1 = document.querySelector('.tab-list-1');
+const scrollLeftButton1 = document.querySelector('.scroll-left-1');
+const scrollRightButton1 = document.querySelector('.scroll-right-1');
 let currentIndex = 0;
 // Thay đổi trong phần JavaScript
-const visibleSongs = 5; // Số lượng bài hát hiển thị cùng một lúc
+const visibleSongs = 1; // Số lượng bài hát hiển thị cùng một lúc
+let currentIndex1 = 0;
+// Thay đổi trong phần JavaScript
+const visibleSongs1 = 1; // Số lượng bài hát hiển thị cùng một lúc
 
+// Thay đổi trong phần JavaScript
+const currentYear = new Date().getFullYear(); // Lấy năm hiện tại
+function scrollToCurrentYear() {
+    for (let i = 0; i < songList1.childElementCount; i++) {
+        const song = songList1.children[i];
+        const songYear = parseInt(song.getAttribute('data-year'), 10);
+
+        if (songYear === currentYear) {
+            currentIndex1 = Math.max(0, i - (i % visibleSongs1)); // Đảm bảo index là bội số của visibleSongs
+            break;
+        }
+    }
+    updateCarousel1();
+}
+// Khai báo biến
+const selectTab = document.querySelector('#select-tab');
+const tabNamHS = document.querySelector('#tab-nam-hs');
+const tabNamHS1 = document.querySelector('#tab-nam-hs-1');
+
+// Xử lý sự kiện change của phần tử select
+selectTab.addEventListener('change', () => {
+    if (selectTab.value === 'tab-nam-hs') {
+        tabNamHS.style.display = 'block';
+        tabNamHS1.style.display = 'none';
+    } else if (selectTab.value === 'tab-nam-hs-1') {
+        tabNamHS.style.display = 'none';
+        tabNamHS1.style.display = 'block';
+        scrollToCurrentYear(); // Cuộn danh sách về năm hiện tại
+    }
+});
+
+// Thêm mã JavaScript để xử lý việc cuộn danh sách bài hát tại đây
 
 // Thay đổi trong phần JavaScript
 scrollLeftButton.addEventListener('click', () => {
@@ -61,3 +99,55 @@ function updateCarousel() {
     scrollRightButton.classList.toggle('hidden', currentIndex >= songList.childElementCount - visibleSongs);
     scrollRightButton.classList.toggle('visible', currentIndex < songList.childElementCount - visibleSongs);
 }
+// Thay đổi trong phần JavaScript
+scrollLeftButton1.addEventListener('click', () => {
+    if (currentIndex1 > 0) {
+        currentIndex1 -= visibleSongs1;
+        if (currentIndex1 < 0) {
+            currentIndex1 = 0;
+        }
+        updateCarousel1();
+    }
+});
+
+scrollRightButton1.addEventListener('click', () => {
+    if (currentIndex1 < songList1.childElementCount - visibleSongs1) {
+        currentIndex1 += visibleSongs1;
+        if (currentIndex1 > songList1.childElementCount - visibleSongs1) {
+            currentIndex1 = songList1.childElementCount - visibleSongs1;
+        }
+        updateCarousel1();
+    }
+});
+
+function updateCarousel1() {
+    const transformValue = -currentIndex1 * 29;
+    songList1.style.transform = `translateX(${transformValue}vh)`;
+
+    // Cập nhật trạng thái nút mũi tên
+    scrollLeftButton1.classList.toggle('hidden', currentIndex1 === 0);
+    scrollLeftButton1.classList.toggle('visible', currentIndex1 !== 0);
+    scrollRightButton1.classList.toggle('hidden', currentIndex1 >= songList1.childElementCount - visibleSongs1);
+    scrollRightButton1.classList.toggle('visible', currentIndex1 < songList1.childElementCount - visibleSongs1);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var optionSelect = document.getElementById("select-tab");
+
+    optionSelect.addEventListener("change", function () {
+        var selectedValue = this.value;
+        var div1 = document.getElementById("tab-nam-hs");
+        var div2 = document.getElementById("tab-nam-hs-1");
+
+        if (selectedValue === "tab-nam-hs") {
+            div1.style.display = "flex";
+            div2.style.display = "none";
+        } else if (selectedValue === "tab-nam-hs-1") {
+            div1.style.display = "none";
+            div2.style.display = "flex";
+        }
+    });
+});
+
+
+
