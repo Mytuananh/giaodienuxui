728a6a5b97e762f2e290080f90e4285fdc00a11e
Mytuananh <tuanbenguyencodon11071973@gmail.com>
2023-04-28 04:43:00 +0700

    fix input test 2

--- test-2.js
+++ test-2.js
@@ -1,38 +1,10 @@
-const searchInput = document.getElementById('searchInput');
-const searchResults = document.getElementById('searchResults');
-const selectedProjects = document.getElementById('selectedProjects');
-
-const projects = [
-    'Công trình A',
-    'Công trình B',
-    'Công trình C',
-    'Công trình D',
-];
-
-searchInput.addEventListener('input', (e) => {
-    const query = e.target.value.toLowerCase();
-    searchResults.innerHTML = '';
-
-    projects.forEach((project) => {
-        if (project.toLowerCase().includes(query)) {
-            const result = document.createElement('div');
-            result.textContent = project;
-            result.addEventListener('click', () => {
-                // Kiểm tra xem công trình đã được chọn chưa
-                const isSelected = Array.from(selectedProjects.options).some(
-                    (option) => option.value === project
-                );
-
-                // Nếu chưa được chọn, thêm công trình vào thẻ <select multiple>
-                if (!isSelected) {
-                    const option = document.createElement('option');
-                    option.value = project;
-                    option.textContent = project;
-                    option.selected = true;
-                    selectedProjects.add(option);
-                }
-            });
-            searchResults.appendChild(result);
-        }
-    });
-});
+function displayFileCount(event) {
+    var input = event.target;
+    var label = input.nextElementSibling;
+    var labelText = label.querySelector('.custom-file-input__text');
+    if (input.files.length == 0) {
+        labelText.innerHTML = 'Chọn tệp';
+    } else {
+        labelText.innerHTML = input.files.length + ' file đã chọn';
+    }
+}
