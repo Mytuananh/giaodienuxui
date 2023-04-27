21fcb55bf19a882f2e74796943b01a32304b48f0
Mytuananh <tuanbenguyencodon11071973@gmail.com>
2023-04-28 04:46:05 +0700

    fix input type file

--- test-2.js
+++ test-2.js
@@ -2,7 +2,7 @@ function displayFileCount(event) {
     var input = event.target;
     var label = input.nextElementSibling;
     var labelText = label.querySelector('.custom-file-input__text');
-    if (input.files.length == 0) {
+    if (input.files.length === 0) {
         labelText.innerHTML = 'Chọn tệp';
     } else {
         labelText.innerHTML = input.files.length + ' file đã chọn';
